b923877a2ab1b6afe6e556dc4565875c63ad9958
Craban <Antipov-sava@yandex.ru>
2022-02-26 13:45:43 +0300

    add img

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 from django.contrib import messages
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 def login(request):
     if request.method == 'POST':
@@ -33,7 +33,8 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 def profile(request):
-    context = {'title': 'GeekShop - Профиль'}
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'GeekShop - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
 
 def logout(request):
